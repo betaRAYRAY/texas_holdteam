a16e66a97c84aa46a4553704ecb8476ef3926401
structix <janek@schoff.it>
2023-11-16 23:19:31 +0100

    Update

--- src/logic/strategy.rs
+++ src/logic/strategy.rs
@@ -2,13 +2,17 @@ use rocket::serde::json::Json;
 
 pub fn decide(_table: Json<crate::models::table::Table>) -> crate::models::bet::Bet {
     println!("Halloxxx");
-    return crate::models::bet::Bet {
-        bet: _table.minimum_bet,
-    };
     // Look over all players and find the one with the name textas hold'team
     let mut cardsvec = Vec::new();
     for player in &_table.players {
         if player.name == "Texas Hold'team" {
+            if _table.minimum_bet <= player.stack / 2 {
+                return crate::models::bet::Bet {
+                    bet: _table.minimum_bet,
+                };
+            } else {
+                return crate::models::bet::Bet { bet: 0 };
+            }
             // If the player has cards, then we can check them
             let cards = player.cards.clone();
             if let Some(cards) = cards {
