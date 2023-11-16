aa5e699c4e03f23c747e1ca0bfd83150f53a78bb
structix <janek@schoff.it>
2023-11-16 22:48:20 +0100

    Fix

--- src/logic/strategy.rs
+++ src/logic/strategy.rs
@@ -1,6 +1,9 @@
 use rocket::serde::json::Json;
 
 pub fn decide(_table: Json<crate::models::table::Table>) -> crate::models::bet::Bet {
+    return crate::models::bet::Bet {
+        bet: _table.minimum_bet,
+    };
     // Look over all players and find the one with the name textas hold'team
     let mut cardsvec = Vec::new();
     for player in &_table.players {
