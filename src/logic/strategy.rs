57c3ecad181bb701bec82f1d84a9f5148bcb936f
Daniel Eisele <daniel_eisele@gmx.de>
2023-11-16 19:38:17 +0100

    Always bet minimum

--- src/logic/strategy.rs
+++ src/logic/strategy.rs
@@ -3,5 +3,5 @@ use rocket::serde::json::Json;
 pub fn decide(_table: Json<crate::models::table::Table>) -> crate::models::bet::Bet {
     // TODO: Add Poker Logic Here... :)
 
-    return crate::models::bet::Bet{bet: 0}
+    return crate::models::bet::Bet{bet: _table.minimum_bet}
  }
