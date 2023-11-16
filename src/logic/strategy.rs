56625c37a35c11f2d7512ef3558c2e8ffd180e76
structix <janek@schoff.it>
2023-11-16 22:40:25 +0100

    Update tempstrat

--- src/logic/strategy.rs
+++ src/logic/strategy.rs
@@ -48,13 +48,48 @@ pub fn decide(_table: Json<crate::models::table::Table>) -> crate::models::bet::
             boolsuit = false;
         }
     }
-
     if boolsuit {
-        sum += 1;
+        sum += 2;
+    }
+
+    let mut sumCommunity = 0;
+    let mut boolsuitCommunity = true;
+    for i in 0..communityCards.len() - 1 {
+        // Add up all card ranks
+        //let card_num = match cardsvec[i].rank {
+        //    crate::models::rank::Rank::A => 14,
+        //    crate::models::rank::Rank::K => 13,
+        //    crate::models::rank::Rank::Q => 12,
+        //    crate::models::rank::Rank::J => 11,
+        //    crate::models::rank::Rank::_10 => 10,
+        //    crate::models::rank::Rank::_9 => 9,
+        //    crate::models::rank::Rank::_8 => 8,
+        //    crate::models::rank::Rank::_7 => 7,
+        //    crate::models::rank::Rank::_6 => 6,
+        //    crate::models::rank::Rank::_5 => 5,
+        //    crate::models::rank::Rank::_4 => 4,
+        //    crate::models::rank::Rank::_3 => 3,
+        //    crate::models::rank::Rank::_2 => 2,
+        //};
+        //sum += card_num;
+
+        if communityCards[i].rank == communityCards[i + 1].rank {
+            // If the two cards are the same, then we can raise
+            sumCommunity += 1;
+        }
+        if communityCards[i].suit != communityCards[communityCards.len() - 1].suit {
+            // If the two cards are the same suit, then we can raise
+            //sum += 1;
+            boolsuitCommunity = false;
+        }
+    }
+
+    if boolsuitCommunity {
+        sumCommunity += 2;
     }
 
     //let normSum = sum / cardslen;
-    if sum >= 3 && cardslen >= 5 {
+    if sum - sumCommunity >= 2 && cardslen >= 4 {
         return crate::models::bet::Bet {
             bet: _table.minimum_raise,
         };
