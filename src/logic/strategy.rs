81ec538f09b3f9a1a995e1383c291f5523ca49bb
Daniel Eisele <daniel_eisele@gmx.de>
2023-11-16 20:12:14 +0100

    Bet slightly more than minimum

--- src/logic/strategy.rs
+++ src/logic/strategy.rs
@@ -3,5 +3,5 @@ use rocket::serde::json::Json;
 pub fn decide(_table: Json<crate::models::table::Table>) -> crate::models::bet::Bet {
     println!("Table: {:?}", _table);
 
-    return crate::models::bet::Bet{bet: _table.minimum_bet}
+    return crate::models::bet::Bet{bet: _table.minimum_bet * 10 / 9}
  }
