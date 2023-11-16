68e3ff240f58067eb65af79a71b8ecc4098d387e
structix <janek@schoff.it>
2023-11-16 23:08:30 +0100

    Update

--- src/logic/strategy.rs
+++ src/logic/strategy.rs
@@ -1,7 +1,7 @@
 use rocket::serde::json::Json;
 
 pub fn decide(_table: Json<crate::models::table::Table>) -> crate::models::bet::Bet {
-    println!("Hallo");
+    println!("Halloxxx");
     return crate::models::bet::Bet {
         bet: _table.minimum_bet,
     };
