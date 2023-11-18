2a585eef409577e34233d992aa7506b311b46eab
KaiP <kap4102@gmail.com>
2023-11-18 10:29:15 +0100

    add nemesis

--- src/logic/strategy.rs
+++ src/logic/strategy.rs
@@ -175,8 +175,40 @@ pub fn decide(_table: Json<crate::models::table::Table>) -> crate::models::bet::
     let us: Option<&Player> = binding.first();
     let us = us.unwrap();
 
-    let min_bet = _table.minimum_bet;
-    let max_bet = us.stack;
+    let nemesis_binding = _table.players.clone().into_iter().filter(|player| player.name == "Team42").collect::<Vec<Player>>();
+    let nemesis: Option<&Player> = nemesis_binding.first();
+
+    let mut nemesis_is_player = false;
+
+    match nemesis {
+       Some(nemesis_player) =>  nemesis_is_player = true,
+       None => nemesis_is_player = false,
+    };
+
+    let nemesis_bet : i32= if nemesis_is_player {
+        let bet = nemesis.unwrap().bet;
+        return crate::models::bet::Bet {bet: bet};
+    } else {
+        let var_name = 0;
+        return crate::models::bet::Bet {bet: var_name};
+    };
+
+    let nemesis_raise : bool;
+
+    if nemesis_is_player {
+        nemesis_raise = nemesis_bet > min_bet;
+    } else {
+        nemesis_raise = false;
+    };
+
+    let nemesis_all_in : bool;
+
+    if nemesis_is_player {
+        nemesis_all_in = nemesis_bet == nemesis.unwrap().stack;
+    } else {
+        nemesis_is_player = false;
+    };
+
 
     let mut bet = 0;
     
